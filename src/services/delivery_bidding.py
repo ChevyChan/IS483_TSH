5457eacabe26648e735cc5e26fe6e9c7c26790a4
ChevyChan <111484440+ChevyChan@users.noreply.github.com>
2023-05-24 13:55:34 +0800

    Create delivery_bidding.py
    Initial - Work in Progress

---
+++ src/services/delivery_bidding.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from datetime import datetime
+import json 
+import uuid
+
+import os
+import sys
+from os import environ
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("dbURL")
+# app.config["SQLALCHEMY_DATABASE_URI"]
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLACHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
+
+db = SQLAlchemy(app)
+
+CORS(app)
+
+# Table for Bidding Event
