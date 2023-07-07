cc51879b7d844880384b691de6f63ee89d582aa9
ChevyChan <111484440+ChevyChan@users.noreply.github.com>
2023-07-07 13:30:11 +0800

    Comment - Google Chat API Function
    Commenting functions that are not in use - Google Chat API

--- src/services/google_chat/google_membership.py
+++ src/services/google_chat/google_membership.py
@@ -14,56 +14,56 @@ SCOPES = 'https://www.googleapis.com/auth/chat.memberships'
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-# Create a membership for members
-def create_membership(google_spaces, user_name):
-    result = service.spaces().members().create(
-        # The space in which to create a membership.
-        parent = 'spaces/' + google_spaces,
+# # Create a membership for members
+# def create_membership(google_spaces, user_name):
+#     result = service.spaces().members().create(
+#         # The space in which to create a membership.
+#         parent = 'spaces/' + google_spaces,
 
-        # Set the Chat app as the entity that gets added to the space.
-        # 'app' is an alias for the Chat app calling the API.
-        body = {
-            'member': {
-              'name':'users/' + user_name,
-              'type': 'HUMAN'
-            }
-        }
-    ).execute()
+#         # Set the Chat app as the entity that gets added to the space.
+#         # 'app' is an alias for the Chat app calling the API.
+#         body = {
+#             'member': {
+#               'name':'users/' + user_name,
+#               'type': 'HUMAN'
+#             }
+#         }
+#     ).execute()
 
-    # Prints details about the created membership.
-    print(result)
+#     # Prints details about the created membership.
+#     print(result)
 
-def get_member(member_name):
-    # Use the service endpoint to call Chat API.
-    result = service.spaces().members().get(
+# def get_member(member_name):
+#     # Use the service endpoint to call Chat API.
+#     result = service.spaces().members().get(
 
-        # The membership to get.
-        #
-        # Replace SPACE with a space name.
-        # Obtain the space name from the spaces resource of Chat API,
-        # or from a space's URL.
-        #
-        # Replace MEMBER with a membership name.
-        # Obtain the membership name from the memberships resource of
-        # Chat API.
-        name='spaces/SPACE/members/'+member_name
+#         # The membership to get.
+#         #
+#         # Replace SPACE with a space name.
+#         # Obtain the space name from the spaces resource of Chat API,
+#         # or from a space's URL.
+#         #
+#         # Replace MEMBER with a membership name.
+#         # Obtain the membership name from the memberships resource of
+#         # Chat API.
+#         name='spaces/SPACE/members/'+member_name
 
-    ).execute()
+#     ).execute()
 
-    # Prints details about the created membership.
-    print(result)
+#     # Prints details about the created membership.
+#     print(result)
 
-def retrieve_list_of_members(space_name):
-    # Use the service endpoint to call Chat API.
-    result = service.spaces().members().list(
+# def retrieve_list_of_members(space_name):
+#     # Use the service endpoint to call Chat API.
+#     result = service.spaces().members().list(
 
-        # The space for which to list memberships.
-        parent = 'spaces/'+space_name,
+#         # The space for which to list memberships.
+#         parent = 'spaces/'+space_name,
 
-        # An OPTIONAL filter that returns only human space members.
-        filter = 'member.type = "HUMAN" AND role = "ROLE_MEMBER"'
+#         # An OPTIONAL filter that returns only human space members.
+#         filter = 'member.type = "HUMAN" AND role = "ROLE_MEMBER"'
 
-    ).execute()
+#     ).execute()
 
-    # Prints details about the created membership.
-    print(result)
+#     # Prints details about the created membership.
+#     print(result)
--- src/services/google_chat/google_messages.py
+++ src/services/google_chat/google_messages.py
@@ -17,151 +17,151 @@ SCOPES = 'https://www.googleapis.com/auth/chat.messages'
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-def create_messages(spaces_name, body_text, upload_file):
-    if(upload_file != ""):
-        try:
-            # Check for file extension of the upload_file
-            file_ext = upload_file.split('.')[1]
+# def create_messages(spaces_name, body_text, upload_file):
+#     if(upload_file != ""):
+#         try:
+#             # Check for file extension of the upload_file
+#             file_ext = upload_file.split('.')[1]
 
-            # Upload a file to Google Chat.
-            media = MediaFileUpload(upload_file, mimetype='image/'+file_ext)
+#             # Upload a file to Google Chat.
+#             media = MediaFileUpload(upload_file, mimetype='image/'+file_ext)
 
-            # Create a message and attach the uploaded file to it.
-            attachment_uploaded = service.media().upload(
+#             # Create a message and attach the uploaded file to it.
+#             attachment_uploaded = service.media().upload(
 
-                # The space to upload the attachment in.
-                #
-                # Replace SPACE with a space name.
-                # Obtain the space name from the spaces resource of Chat API,
-                # or from a space's URL.
-                parent='spaces/' + spaces_name,
+#                 # The space to upload the attachment in.
+#                 #
+#                 # Replace SPACE with a space name.
+#                 # Obtain the space name from the spaces resource of Chat API,
+#                 # or from a space's URL.
+#                 parent='spaces/' + spaces_name,
 
-                # The filename of the attachment, including the file extension.
-                body={'filename': upload_file},
+#                 # The filename of the attachment, including the file extension.
+#                 body={'filename': upload_file},
 
-                # Media resource of the attachment.
-                media_body=media
+#                 # Media resource of the attachment.
+#                 media_body=media
 
-            ).execute()
+#             ).execute()
 
-            print(attachment_uploaded)
+#             print(attachment_uploaded)
 
-            # Create a Chat message with attachment.
-            result = service.spaces().messages().create(
-                # The space to create the message in.
-                #
-                # Replace SPACE with a space name.
-                # Obtain the space name from the spaces resource of Chat API,
-                # or from a space's URL.
-                #
-                # Must match the space name that the attachment is uploaded to.
-                parent='spaces/'+spaces_name,
+#             # Create a Chat message with attachment.
+#             result = service.spaces().messages().create(
+#                 # The space to create the message in.
+#                 #
+#                 # Replace SPACE with a space name.
+#                 # Obtain the space name from the spaces resource of Chat API,
+#                 # or from a space's URL.
+#                 #
+#                 # Must match the space name that the attachment is uploaded to.
+#                 parent='spaces/'+spaces_name,
 
-                # The message to create.
-                body={
-                    'text': body_text,
-                    'attachment': [attachment_uploaded]
-                }
+#                 # The message to create.
+#                 body={
+#                     'text': body_text,
+#                     'attachment': [attachment_uploaded]
+#                 }
 
-            ).execute()
+#             ).execute()
 
-            print(result)
-            return result
-        except HTTPError as err:
-            print(err)
-    else:
-        try:
-            # Create a Chat message if no attachment.
-            result = service.spaces().messages().create(
+#             print(result)
+#             return result
+#         except HTTPError as err:
+#             print(err)
+#     else:
+#         try:
+#             # Create a Chat message if no attachment.
+#             result = service.spaces().messages().create(
 
-            # The space to create the message in.
-            #
-            # Replace SPACE with a space name.
-            # Obtain the space name from the spaces resource of Chat API,
-            # or from a space's URL.
-            parent='spaces/'+spaces_name,
+#             # The space to create the message in.
+#             #
+#             # Replace SPACE with a space name.
+#             # Obtain the space name from the spaces resource of Chat API,
+#             # or from a space's URL.
+#             parent='spaces/'+spaces_name,
 
-            # The message to create.
-            body={'text': body_text}
+#             # The message to create.
+#             body={'text': body_text}
 
-            ).execute()
+#             ).execute()
 
-            print(result)
-            return result
-        except HTTPError as err:
-            print(err)
+#             print(result)
+#             return result
+#         except HTTPError as err:
+#             print(err)
 
-# Get messages and attachments (if any) by caching into DB first and then, retrieve message from DB
-def get_messages(spaces_name, body_text, body_attachment):
-    if(body_attachment != ""):
-        # Get a Chat message.
-        result = service.spaces().messages().attachments().get(
+# # Get messages and attachments (if any) by caching into DB first and then, retrieve message from DB
+# def get_messages(spaces_name, body_text, body_attachment):
+#     if(body_attachment != ""):
+#         # Get a Chat message.
+#         result = service.spaces().messages().attachments().get(
 
-            # The message to get.
-            #
-            # Replace SPACE with a space name.
-            # Obtain the space name from the spaces resource of Chat API,
-            # or from a space's URL.
-            #
-            # Replace MESSAGE with a message name.
-            # Obtain the message name from the response body returned
-            # after creating a message asynchronously with Chat REST API.
-            name='spaces/' + spaces_name + '/messages/' + body_text + '/attachments/' + body_attachment
+#             # The message to get.
+#             #
+#             # Replace SPACE with a space name.
+#             # Obtain the space name from the spaces resource of Chat API,
+#             # or from a space's URL.
+#             #
+#             # Replace MESSAGE with a message name.
+#             # Obtain the message name from the response body returned
+#             # after creating a message asynchronously with Chat REST API.
+#             name='spaces/' + spaces_name + '/messages/' + body_text + '/attachments/' + body_attachment
 
-        ).execute()
+#         ).execute()
 
-        # Print Chat API's response in your command line interface.
-        print(result)
-    else:
-        # Use the service endpoint to call Chat API.
-        result = service.spaces().messages().get(
+#         # Print Chat API's response in your command line interface.
+#         print(result)
+#     else:
+#         # Use the service endpoint to call Chat API.
+#         result = service.spaces().messages().get(
 
-            # The message to get.
-            #
-            # Replace SPACE with a space name.
-            # Obtain the space name from the spaces resource of Chat API,
-            # or from a space's URL.
-            #
-            # Replace MESSAGE with a message name.
-            # Obtain the message name from the response body returned
-            # after creating a message asynchronously with Chat REST API.
-            name = 'spaces/' + spaces_name + '/messages/' + body_text
+#             # The message to get.
+#             #
+#             # Replace SPACE with a space name.
+#             # Obtain the space name from the spaces resource of Chat API,
+#             # or from a space's URL.
+#             #
+#             # Replace MESSAGE with a message name.
+#             # Obtain the message name from the response body returned
+#             # after creating a message asynchronously with Chat REST API.
+#             name = 'spaces/' + spaces_name + '/messages/' + body_text
 
-        ).execute()
+#         ).execute()
 
-        # Prints details about the created membership.
-        print(result)
+#         # Prints details about the created membership.
+#         print(result)
 
-def get_list_messages(spaces_name, filter_date_time):
-    # Use the service endpoint to call Chat API.
-    result = service.spaces().messages().list(
+# def get_list_messages(spaces_name, filter_date_time):
+#     # Use the service endpoint to call Chat API.
+#     result = service.spaces().messages().list(
 
-          # The space for which to list messages.
-          parent = 'spaces/'+spaces_name,
+#           # The space for which to list messages.
+#           parent = 'spaces/'+spaces_name,
 
-          # An optional filter that returns messages
-          # created after March 16, 2023.
-          filter = 'createTime > ' + filter_date_time 
+#           # An optional filter that returns messages
+#           # created after March 16, 2023.
+#           filter = 'createTime > ' + filter_date_time 
 
-      ).execute()
+#       ).execute()
 
-    # Prints details about the created membership.
-    print(result)
+#     # Prints details about the created membership.
+#     print(result)
 
-def download_attachment(RESOURCE_NAME):
-    # Download media resource.
-    request = service.media().download_media(
-        resourceName=RESOURCE_NAME,
-    )
-    file = io.BytesIO()
-    downloader = MediaIoBaseDownload(file, request)
+# def download_attachment(RESOURCE_NAME):
+#     # Download media resource.
+#     request = service.media().download_media(
+#         resourceName=RESOURCE_NAME,
+#     )
+#     file = io.BytesIO()
+#     downloader = MediaIoBaseDownload(file, request)
 
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        if status.total_size:
-            print(f'Total size: {status.total_size}')
-        print(f'Download {int(status.progress() * 100)}')
+#     done = False
+#     while done is False:
+#         status, done = downloader.next_chunk()
+#         if status.total_size:
+#             print(f'Total size: {status.total_size}')
+#         print(f'Download {int(status.progress() * 100)}')
 
 
-create_messages('AAAAouZ1mcE', "Hello World!", "")
+# create_messages('AAAAouZ1mcE', "Hello World!", "")
